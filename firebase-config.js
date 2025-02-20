cde3be963dee527e18d78778d486287de62cff30
abhiyeduru <abhiyeduru8@gmail.com>
2025-02-20 10:10:03 +0530

    abhi

--- firebase-config.js
+++ firebase-config.js
@@ -9,5 +9,6 @@ const firebaseConfig = {
   appId: "1:1036695376596:web:949fac20be565401d1a141",
   measurementId: "G-796H57CJWH"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
